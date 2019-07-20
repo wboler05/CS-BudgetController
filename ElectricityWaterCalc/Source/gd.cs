27db513b3088879ce5202a0f13a0af8a7af4c2b2
wboler <wboler@iupui.edu>
2019-07-20 11:18:42 -0400

    Adds gradient descent controller

---
+++ ElectricityWaterCalc/Source/gd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BudgetManagerMod
+{
+    public class GD
+    {
+
+        public double response(double error)
+        {
+            double output = - m_gain * error;
+            return output;
+        }
+
+        public double m_gain;
+
+        public GD(double gain)
+        {
+            m_gain = gain;
+        }
+    }
+}
